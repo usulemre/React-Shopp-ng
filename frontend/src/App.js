75109536a697083847d4230ecff78c8e6ab3802d
emre usul <emreusul83@gmail.com>
2024-03-22 18:58:11 +0300

    created Store (createContext)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,35 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import HomeScreen from "./screen/HomeScreen";
 import ProductScreen from "./screen/ProductScreen";
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 import { LinkContainer } from "react-router-bootstrap";
+import { Badge, Nav } from "react-bootstrap";
+import { useContext } from "react";
+import { Store } from "./Store";
 function App() {
+  const { state } = useContext(Store);
+  const {
+    cart: { cartItems },
+  } = state;
   return (
     <BrowserRouter>
-      <div className='d-flex flex-column min-vh-100 min'>
+      <div className="d-flex flex-column min-vh-100 min">
         <header>
           <Navbar bg="dark" variant="dark">
             <Container>
               <LinkContainer to="/">
                 <Navbar.Brand href="#home">Shopping</Navbar.Brand>
               </LinkContainer>
+              <Nav className="justify-content-end">
+                <Link to="cart" className="nav-link">
+                  Sepet
+                  {cartItems.length > 0 && (
+                    <Badge pill bg="danger">{cartItems.length}</Badge>
+                  )}
+                </Link>
+              </Nav>
             </Container>
           </Navbar>
         </header>
--- frontend/src/components/Products.js
+++ frontend/src/components/Products.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Button, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import Raiting from "./Raiting";
+import { Store } from "../Store";
 
 function Products(props) {
   const { product } = props;
+  const { state, dispatch: crxDispatch } = useContext(Store);
+
+  const addCartHandle = () => {
+    crxDispatch({
+      type: "CART_ADD_ITEM",
+      payload: { ...product, quantity: 1 },
+    });
+  };
+  
   return (
     <Card sm={6} md={4} lg={3} className="mb-3">
       <Link to={`/product/${product.slug}`}>
@@ -21,7 +31,9 @@ function Products(props) {
         </Link>
         <Raiting rating={product.rating} numReviews={product.numReviews} />
         <Card.Text>{product.price}TL</Card.Text>
-        <Button className="btn-warning">Karta Ekle</Button>
+        <Button onClick={addCartHandle} className="btn-warning">
+          Karta Ekle
+        </Button>
       </Card.Body>
     </Card>
   );
